c33f3c7f827732300dc23c32cb80aaf65880d0e1
Pittab <120305267+Pittab@users.noreply.github.com>
2023-03-24 10:36:40 +0000

    Modifications to make communication more precise and also auto-forward

--- main.ts
+++ main.ts
@@ -1,20 +1,21 @@
 input.onButtonPressed(Button.A, function () {
     radio.setGroup(1)
-    radio.sendString("Ok")
+    radio.sendString("(user):Ok")
 })
 radio.onReceivedString(function (receivedString) {
     music.playTone(988, music.beat(BeatFraction.Whole))
     Message = receivedString
+    radio.sendString(receivedString)
     basic.showString(Message)
 })
 input.onButtonPressed(Button.B, function () {
     radio.setGroup(1)
-    radio.sendString("Nearby")
+    radio.sendString("(user:)Nearby")
 })
 input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 1e+31; index++) {
         radio.setGroup(1)
-        radio.sendString("SOS")
+        radio.sendString("(User):SOS")
         music.playMelody("A - A - A - - - ", 605)
         music.playMelody("A - A - A - - - ", 179)
         music.playMelody("A - A - A - - - ", 605)
@@ -22,7 +23,7 @@ input.onGesture(Gesture.Shake, function () {
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     radio.setGroup(1)
-    radio.sendString("SOS")
+    radio.sendString("(user):SOS")
     music.playMelody("A - A - A - - - ", 605)
     music.playMelody("A - A - A - - - ", 179)
     music.playMelody("A - A - A - - - ", 605)
